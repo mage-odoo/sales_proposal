1fbc470a95714c74b7fa1d77dd7640cc724211b6
Manthan Gehlot <mage@odoo.com>
2023-01-23 15:26:04 +0530

    [ADD] sales proposal module

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Sales Proposal',
+    'version': '1.0.0',
+    'category': 'Sales',
+    'author': ' Odoo S.A.',
+    'summary': 'Sales Proposal',
+    'description': """To Add Functional Specification For Sales Menu""",
+    'depends': [
+        'sale_management'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'data/ir_sequence_data.xml',
+
+        'views/sale_order_views.xml',
+        'views/sale.xml',
+    ],
+    'demo': [],
+    'sequence': -300,
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
---
+++ models/sale_proposal.py
@@ -0,0 +1,379 @@
+from odoo import models, fields, api, _
+
+
+class SaleProposal(models.Model):
+    _name = 'sale.proposal'
+    _description = "Sales Proposal"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = "name"
+    name = fields.Char(
+        string="Order Reference",
+        required=True, copy=False, readonly=True,
+        index='trigram',
+        states={'draft': [('readonly', False)]},
+        default=lambda self: _('New'))
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        required=True, index=True,
+        default=lambda self: self.env.company)
+    pricelist_id = fields.Many2one(
+        comodel_name='product.pricelist',
+        string="Pricelist",
+        compute='_compute_pricelist_id',
+        # Unrequired company
+        store=True, readonly=False, precompute=True, check_company=True, required=True,
+        tracking=1,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        help="If you change the pricelist, only newly added lines will be affected.")
+    currency_id = fields.Many2one(
+        related='pricelist_id.currency_id',
+        depends=["pricelist_id"],
+        store=True, precompute=True, ondelete="restrict")
+    currency_rate = fields.Float(
+        string="Currency Rate",
+        compute='_compute_currency_rate',
+        digits=(12, 6),
+        store=True, precompute=True)
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string="Customer",
+        required=True, readonly=False, change_default=True, index=True)
+    state = fields.Selection(
+        selection=[
+            ('draft', "Draft"),
+            ('send', "Send"),
+            ('confirm', "Confirm")
+        ],
+        string="Status",
+        default='draft', required=True)
+    validity_date = fields.Date(
+        string="Expiration",
+        compute='_compute_validity_date',
+        store=True, readonly=False, copy=False, precompute=True)
+    date_order = fields.Datetime(
+        string="Order Date",
+        required=True, readonly=False, copy=False,
+        help="Creation date of draft/sent orders,\nConfirmation date of confirmed orders.",
+        default=fields.Datetime.now)
+    payment_term_id = fields.Many2one(
+        comodel_name='account.payment.term',
+        string="Payment Terms",
+        compute='_compute_payment_term_id',
+        store=True, readonly=False, precompute=True, check_company=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    date_of_proposal = fields.Datetime(
+        string="Order Date",
+        required=True, readonly=False, copy=False,
+        help="Creation date of draft/sent orders,\nConfirmation date of confirmed orders.",
+        default=fields.Datetime.now)
+    order_line = fields.One2many(
+        comodel_name='sale.order.line',
+        inverse_name='order_id',
+        string="Order Lines",
+        copy=True, auto_join=True)
+    tax_country_id = fields.Many2one(
+        comodel_name='res.country',
+        compute='_compute_tax_country_id',
+        # Avoid access error on fiscal position when reading a sale order with company != user.company_ids
+        compute_sudo=True)  # used to filter available taxes depending on the fiscal country and position
+    fiscal_position_id = fields.Many2one(
+        comodel_name='account.fiscal.position',
+        string="Fiscal Position",
+        compute='_compute_fiscal_position_id',
+        store=True, readonly=False, precompute=True, check_company=True,
+        help="Fiscal positions are used to adapt taxes and accounts for particular customers or sales orders/invoices."
+        "The default value comes from the customer.",
+        domain="[('company_id', '=', company_id)]")
+    partner_shipping_id = fields.Many2one(
+        comodel_name='res.partner',
+        string="Delivery Address",
+        compute='_compute_partner_shipping_id',
+        store=True, readonly=False, required=True, precompute=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    tax_totals = fields.Binary(compute='_compute_tax_totals', exportable=False)
+    amount_total = fields.Monetary(
+        string="Total", store=True, compute='_compute_amounts', tracking=4)
+    amount_untaxed = fields.Monetary(
+        string="Untaxed Amount", store=True, compute='_compute_amounts', tracking=5)
+    amount_tax = fields.Monetary(
+        string="Taxes", store=True, compute='_compute_amounts')
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string="Salesperson",
+        compute='_compute_user_id',
+        store=True, readonly=False, precompute=True, index=True,
+        tracking=2,
+        domain=lambda self: "[('groups_id', '=', {}), ('share', '=', False), ('company_ids', '=', company_id)]".format(
+            self.env.ref("sales_team.group_sale_salesman").id
+        ))
+    team_id = fields.Many2one(
+        comodel_name='crm.team',
+        string="Sales Team",
+        compute='_compute_team_id',
+        store=True, readonly=False, precompute=True, ondelete="set null",
+        change_default=True, check_company=True,  # Unrequired company
+        tracking=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    reference = fields.Char(
+        string="Payment Ref.",
+        help="The payment communication of this sale order.",
+        copy=False)
+    client_order_ref = fields.Char(string="Customer Reference", copy=False)
+    analytic_account_id = fields.Many2one(
+        comodel_name='account.analytic.account',
+        string="Analytic Account",
+        copy=False, check_company=True,  # Unrequired company
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    require_signature = fields.Boolean(
+        string="Online Signature",
+        compute='_compute_require_signature',
+        store=True, readonly=False, precompute=True,
+        help="Request a online signature and/or payment to the customer in order to confirm orders automatically.")
+    require_payment = fields.Boolean(
+        string="Online Payment",
+        compute='_compute_require_payment',
+        store=True, readonly=False, precompute=True,)
+    commitment_date = fields.Datetime(
+        string="Delivery Date", copy=False,
+        help="This is the delivery date promised to the customer. "
+             "If set, the delivery order will be scheduled based on "
+             "this date rather than product lead times.")
+    expected_date = fields.Datetime(
+        string="Expected Date",
+        # Note: can not be stored since depends on today()
+        compute='_compute_expected_date', store=False,
+        help="Delivery date you can promise to the customer, computed from the minimum lead time of the order lines.")
+    origin = fields.Char(
+        string="Source Document",
+        help="Reference of the document that generated this sales order request")
+    # campaign_id = fields.Many2one()
+    # medium_id = fields.Many2one(ondelete='set null')
+    # source_id = fields.Many2one(ondelete='set null')
+    show_update_pricelist = fields.Boolean(
+        string="Has Pricelist Changed", store=False)  # True if the pricelist was changed
+
+    @ api.depends('order_line.customer_lead', 'date_order', 'state')
+    def _compute_expected_date(self):
+        """ For service and consumable, we only take the min dates. This method is extended in sale_stock to
+            take the picking_policy of SO into account.
+        """
+        self.mapped("order_line")  # Prefetch indication
+        for order in self:
+            if order.state == 'cancel':
+                order.expected_date = False
+                continue
+            dates_list = order.order_line.filtered(
+                lambda line: not line.display_type and not line._is_delivery()
+            ).mapped(lambda line: line and line._expected_date())
+            if dates_list:
+                order.expected_date = min(dates_list)
+            else:
+                order.expected_date = False
+
+    @ api.depends('company_id')
+    def _compute_require_signature(self):
+        for order in self:
+            order.require_signature = order.company_id.portal_confirmation_sign
+
+    @ api.depends('company_id')
+    def _compute_require_payment(self):
+        for order in self:
+            order.require_payment = order.company_id.portal_confirmation_pay
+
+    @ api.depends('partner_id')
+    def _compute_partner_invoice_id(self):
+        for order in self:
+            order.partner_invoice_id = order.partner_id.address_get(
+                ['invoice'])['invoice'] if order.partner_id else False
+
+    @ api.depends('partner_id', 'user_id')
+    def _compute_team_id(self):
+        cached_teams = {}
+        for order in self:
+            default_team_id = self.env.context.get(
+                'default_team_id', False) or order.team_id.id or order.partner_id.team_id.id
+            user_id = order.user_id.id
+            company_id = order.company_id.id
+            key = (default_team_id, user_id, company_id)
+            if key not in cached_teams:
+                cached_teams[key] = self.env['crm.team'].with_context(
+                    default_team_id=default_team_id
+                )._get_default_team_id(
+                    user_id=user_id, domain=[('company_id', 'in', [company_id, False])])
+            order.team_id = cached_teams[key]
+
+    @ api.depends('partner_id')
+    def _compute_user_id(self):
+        for order in self:
+            if not order.user_id:
+                order.user_id = order.partner_id.user_id or order.partner_id.commercial_partner_id.user_id or self.env.user
+
+    @ api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'company_id' in vals:
+                self = self.with_company(vals['company_id'])
+            if vals.get('name', _("New")) == _("New"):
+                seq_date = fields.Datetime.context_timestamp(
+                    self, fields.Datetime.to_datetime(vals['date_order'])
+                ) if 'date_order' in vals else None
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                    'sale.proposal', sequence_date=seq_date) or _("New")
+
+        return super().create(vals_list)
+
+    @ api.depends('order_line.price_subtotal', 'order_line.price_tax', 'order_line.price_total')
+    def _compute_amounts(self):
+        """Compute the total amounts of the SO."""
+        for order in self:
+            order_lines = order.order_line.filtered(
+                lambda x: not x.display_type)
+            order.amount_untaxed = sum(order_lines.mapped('price_subtotal'))
+            order.amount_total = sum(order_lines.mapped('price_total'))
+            order.amount_tax = sum(order_lines.mapped('price_tax'))
+
+    @ api.depends('company_id', 'fiscal_position_id')
+    def _compute_tax_country_id(self):
+        for record in self:
+            if record.fiscal_position_id.foreign_vat:
+                record.tax_country_id = record.fiscal_position_id.country_id
+            else:
+                record.tax_country_id = record.company_id.account_fiscal_country_id
+
+    @ api.depends('order_line.tax_id', 'order_line.price_unit', 'amount_total', 'amount_untaxed', 'currency_id')
+    def _compute_tax_totals(self):
+        for order in self:
+            order_lines = order.order_line.filtered(
+                lambda x: not x.display_type)
+            order.tax_totals = self.env['account.tax']._prepare_tax_totals(
+                [x._convert_to_tax_base_line_dict() for x in order_lines],
+                order.currency_id or order.company_id.currency_id,
+            )
+
+    @ api.depends('partner_id')
+    def _compute_partner_shipping_id(self):
+        for order in self:
+            order.partner_shipping_id = order.partner_id.address_get(
+                ['delivery'])['delivery'] if order.partner_id else False
+
+    @ api.depends('company_id')
+    def _compute_validity_date(self):
+        enabled_feature = bool(self.env['ir.config_parameter'].sudo(
+        ).get_param('sale.use_quotation_validity_days'))
+        if not enabled_feature:
+            self.validity_date = False
+            return
+
+    @ api.depends('partner_id')
+    def _compute_payment_term_id(self):
+        for order in self:
+            order = order.with_company(order.company_id)
+            order.payment_term_id = order.partner_id.property_payment_term_id
+
+    @ api.depends('partner_id')
+    def _compute_pricelist_id(self):
+        for order in self:
+            if not order.partner_id:
+                order.pricelist_id = False
+                continue
+            order = order.with_company(order.company_id)
+            order.pricelist_id = order.partner_id.property_product_pricelist
+
+    @ api.depends('currency_id', 'date_order', 'company_id')
+    def _compute_currency_rate(self):
+        cache = {}
+        for order in self:
+            order_date = order.date_order.date()
+            if not order.company_id:
+                order.currency_rate = order.currency_id.with_context(
+                    date=order_date).rate or 1.0
+                continue
+            elif not order.currency_id:
+                order.currency_rate = 1.0
+            else:
+                key = (order.company_id.id, order_date, order.currency_id.id)
+                if key not in cache:
+                    cache[key] = self.env['res.currency']._get_conversion_rate(
+                        from_currency=order.company_id.currency_id,
+                        to_currency=order.currency_id,
+                        company=order.company_id,
+                        date=order_date,
+                    )
+                order.currency_rate = cache[key]
+
+    @ api.depends('partner_shipping_id', 'partner_id', 'company_id')
+    def _compute_fiscal_position_id(self):
+        """
+        Trigger the change of fiscal position when the shipping address is modified.
+        """
+        cache = {}
+        for order in self:
+            if not order.partner_id:
+                order.fiscal_position_id = False
+                continue
+            key = (order.company_id.id, order.partner_id.id,
+                   order.partner_shipping_id.id)
+            if key not in cache:
+                cache[key] = self.env['account.fiscal.position'].with_company(
+                    order.company_id
+                )._get_fiscal_position(order.partner_id, order.partner_shipping_id)
+            order.fiscal_position_id = cache[key]
+
+    def send_proposal_mail(self):
+        pass
+
+    def sale_proposal_confirm(self):
+        print("function called  ")
+
+    def action_preview_sale_proposal(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': self.get_portal_url(),
+        }
+
+    def action_proposal_send(self):
+        """ Opens a wizard to compose an email, with relevant mail template loaded by default """
+        self.ensure_one()
+        self.order_line._validate_analytic_distribution()
+        lang = self.env.context.get('lang')
+        mail_template = self._find_mail_template()
+        if mail_template and mail_template.lang:
+            lang = mail_template._render_lang(self.ids)[self.id]
+        ctx = {
+            'default_model': 'sale.proposal',
+            'default_res_id': self.id,
+            'default_use_template': bool(mail_template),
+            'default_template_id': mail_template.id if mail_template else None,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True,
+            'model_description': self.with_context(lang=lang),
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': ctx,
+        }
+
+    def _find_mail_template(self):
+        """ Get the appropriate mail template for the current sales order based on its state.
+
+        If the SO is confirmed, we return the mail template for the sale confirmation.
+        Otherwise, we return the quotation email template.
+
+        :return: The correct mail template based on the current status
+        :rtype: record of `mail.template` or `None` if not found
+        """
+        self.ensure_one()
+        if self.env.context.get('proforma') or self.state not in ('sale', 'done'):
+            return self.env.ref('sales_proposal.email_template_edi_proposal', raise_if_not_found=False)
+        else:
+            return self._get_confirmation_template()
