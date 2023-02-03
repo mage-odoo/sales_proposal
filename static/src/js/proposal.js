c89c560d6bac41578a38f20ae1e2f38dc02fcca0
Manthan Gehlot <mage@odoo.com>
2023-02-03 16:07:44 +0530

    [IMP] improve compres JavaScript

--- static/src/js/proposal.js
+++ static/src/js/proposal.js
@@ -4,46 +4,27 @@ import { useService } from "@web/core/utils/hooks";
 var ajax = require('web.ajax');
 
 publicWidget.registry.SaleProposal = publicWidget.Widget.extend({
-    
   setup(){this.dialogService = useService("dialog");},
     selector: '.o_portal_sale_proposal_sidebar',
     events: {
-      'change #sales_proposal_qty': '_onQty',
-      'change #sales_proposal_price_unit': '_onPriceUnit',
+      'change #update_val': '_onUpdateQtyUnit',
     },
-    
-    _onQty: function (ev) {
-      let self = this,$target = $(ev.currentTarget);
-      var $updatedQty = parseFloat($target.val());
-      var $line_id = $target.attr('data-order-id');
-      var $proposal_id = $target.attr('proposal-id');
-      const access_token = new URLSearchParams(window.location.search).get('access_token');
-      var dict = {
+  _onUpdateQtyUnit: function (ev) { 
+    let self = this,$target = $(ev.currentTarget);
+    var $updatedQty = parseFloat($target.val());
+    var $line_id = $target.attr('data-order-id');
+    var $proposal_id = $target.attr('proposal-id');
+    var $field = $target.attr('updatefield');
+    const access_token = new URLSearchParams(window.location.search).get('access_token');
+    var dict = {
         'line_id': $line_id,
-        'field':'product_uom_qty',
+        'field': $field,
         'value': $updatedQty,
         'access_token': access_token,
         'proposal_id':$proposal_id 
       };
       self._updateElement("qty", dict);
-    },
-
-    _onPriceUnit: function (ev) {
-      let self = this,$target = $(ev.currentTarget);
-      var $updatedUnitPrice = parseFloat($target.val());
-      var $line_id = $target.attr('data-order-id');
-      var $proposal_id = $target.attr('proposal-id');
-      const access_token = new URLSearchParams(window.location.search).get('access_token');
-      var dict = {
-        'line_id': $line_id,
-        'field':'price_unit',
-        'value': $updatedUnitPrice,
-        'access_token': access_token,
-        'proposal_id':$proposal_id 
-      };
-      self._updateElement("UnitPrice", dict);
-    },
-
+  },
     _updateElement: function (perameter,dict) {
       var done=ajax.jsonRpc('/my/proposal/'+dict['proposal_id']+'/update', 'call', {'data': dict, 'access_token':dict['access_token']});
         done.then(setTimeout(function(){ location.reload(); }, 200))
